20083634d15f68eb2eae744429101f82682cf223
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2026-03-07 21:54:43 -0300

    Style(sobre): Adds a decorative yellow ball to the upper right corner of the header.

--- src/pages/SobrePage.jsx
+++ src/pages/SobrePage.jsx
@@ -4,7 +4,13 @@ const SobrePage = () => {
   return (
     <div className="bg-slate-50">
       {/* Header  */}
-      <header className="bg-slate-800 min-h-[400px] text-white relative overflow-hidden"></header>
+      <header className="bg-slate-800 min-h-[400px] text-white relative overflow-hidden">
+        {/* Bolas decorativas amarelas */}
+        <div className="absolute inset-0 opacity-10">
+          {/* Bola 1 - está à direita no topo do header */}
+          <div className="absolute top-0 right-0 w-96 h-96 bg-amber-400 rounded-full -translate-y-1/2 translate-x-1/2" />
+        </div>
+      </header>
     </div>
   );
 };
